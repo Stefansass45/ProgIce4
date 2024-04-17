927c95ecad96c9e893844bc9b66f7b22d9346327
Stefan <st10061145@vcconnect.edu.za>
2024-04-17 14:15:22 +0200

    migrated models from Winforms

--- wpfLogin/MainWindow.xaml.cs
+++ wpfLogin/MainWindow.xaml.cs
@@ -21,24 +21,77 @@ namespace wpfLogin
             InitializeComponent();
         }
 
-        private void btnLog_Click(object sender, RoutedEventArgs e)
+        private void btnReg_Click(object sender, RoutedEventArgs e)
         {
+            if (LoginDB.isUsernameAvailable(RegUsername.Text))
+            {
+                User newUser = new User(RegUsername.Text, RegPassword.Text);
+                RegUsername.Clear();
+                RegPassword.Clear();
+
+                if (LoginDB.addNewUser(newUser) == newUser)
+                {
+                    LoginDB.SaveToCSVFile("users.csv");
+                    MessageBox.Show($"Registration completed!\nUsername : {newUser.getUsername()}", "Success");
+                    logUsername.Clear();
+                    logPassword.Clear();
+                    loginPanel.Visibility = Visibility.Visible;
+                    registerPanel.Visibility = Visibility.Hidden;
+
+                }
+                else
+                {
+                    MessageBox.Show("Unable to complete registration...", "Failure");
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Username has already been used\nPlease use another username", "Invalid Username");
+            }
 
         }
 
         private void lblReg_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
+            registerPanel.Visibility = Visibility.Visible;
+            loginPanel.Visibility = Visibility.Hidden;
+            RegUsername.Clear();
+            RegPassword.Clear();
         }
 
-        private void btnLog_Click_1(object sender, RoutedEventArgs e)
+        private void btnLog_Click(object sender, RoutedEventArgs e)
         {
+            bool userLoginVerified = false;
 
+            if ((logUsername.Text != String.Empty) && (logPassword.Text != string.Empty))
+            {
+                userLoginVerified = LoginDB.verifyUser(new User(logUsername.Text, logPassword.Text));
+                logUsername.Clear();
+                logPassword.Clear();
+            }
+
+            if (userLoginVerified)
+            {
+                /*FrmDash formDash = new FrmDash();
+                this.Hide();
+                formDash.ShowDialog();*/
+                LoginDB.currentLoggedOnUser = null;
+                this.Show();
+                loginPanel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                MessageBox.Show("Invalid login details provided", "Login Failed");
+            }
         }
 
         private void lblLog_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
+            loginPanel.Visibility = Visibility.Visible;
+            registerPanel.Visibility = Visibility.Hidden;
+            logUsername.Clear();
+            logPassword.Clear();
         }
     }
 }
